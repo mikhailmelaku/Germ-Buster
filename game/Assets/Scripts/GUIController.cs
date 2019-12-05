e1128f2ca05b85bfaa01452a4a3c2c1851b5dd22
mikhailmelaku <mikhailmelaku11@gmail.com>
2019-12-04 18:36:05 -0800

    functioning overworld and bridges to first 4 levels

--- game/Assets/Scripts/GUIController.cs
+++ game/Assets/Scripts/GUIController.cs
@@ -2,28 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GUIController : MonoBehaviour
 {
     public GameObject interfaceDisplay;
+
     public GameObject healthbar;
     public Image healthbarImage;
     public float healthPercent = 100f;
-    
 
-    // Start is called before the first frame update
-    void Awake()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    
+    public GameObject transitionScreen;
+    public Image transitionScreenImage;
+    private float transitionSeconds = 3f;
+    private bool transitionCompleted;
     public void DamageAnimation() {
         // finds the objects it needs to act on (healthbar in this case)
         interfaceDisplay = GameObject.Find("GUI");
@@ -33,7 +25,7 @@ public class GUIController : MonoBehaviour
         // this is code that updates gui to reflect current health.
         if (healthbarImage.color.a > 0.1) {
             Color tempColor = healthbarImage.color;
-            tempColor -= new Color(0, 0, 0, 0.1f);
+            tempColor -= new Color(0, 0.1f, 0.1f, 0);
             healthbarImage.color = tempColor;
         }
         
@@ -41,5 +33,19 @@ public class GUIController : MonoBehaviour
         
     }
 
-    
+    public void Transition() {
+        StartCoroutine(AnimateTransition(transitionSeconds));
+    }
+
+    private IEnumerator AnimateTransition(float time) {
+
+        float rate = 1 / time;
+        Color tempColor = new Color(0, 0, 0, rate);
+
+        for (int i = 1; i < 100; i++) {
+            transitionScreenImage.color += tempColor;
+            yield return new WaitForSeconds(time / 100);
+        }
+        SceneManager.LoadScene("Level1");
+    }
 }
