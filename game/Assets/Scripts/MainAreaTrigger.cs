9d27a816acc710d7cd57414a6e65c332c0a47ed2
mikhailmelaku <mikhailmelaku11@gmail.com>
2020-01-30 15:02:12 -0800

    basic skeleton of level1

---
+++ game/Assets/Scripts/MainAreaTrigger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class MainAreaTrigger : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject cam;
+
+    private void OnTriggerEnter2D(Collider2D coll) {
+        if (coll.CompareTag("Player"))
+            cam.gameObject.GetComponent<CinemachineVirtualCamera>().m_Lens.OrthographicSize = 12;
+    }
+
+    private void OnTriggerExit2D(Collider2D coll) {
+        if (coll.CompareTag("Player"))
+            cam.gameObject.GetComponent<CinemachineVirtualCamera>().m_Lens.OrthographicSize = 6;
+    }
+}
