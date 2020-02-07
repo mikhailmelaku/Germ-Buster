4311bf914460d8ed5438b8d4600d88aa03adfe77
mikhailmelaku <mikhailmelaku11@gmail.com>
2020-02-06 17:34:10 -0800

    player crouch, camera movement

--- game/Assets/Scripts/PlayerScripts/PlayerMovement.cs
+++ game/Assets/Scripts/PlayerScripts/PlayerMovement.cs
@@ -18,6 +18,7 @@ public class PlayerMovement : MonoBehaviour
     private bool canDoubleJump;
     private bool grounded = true;
     private bool jumpInputted;
+    private bool crouching;
     private Vector2 direction;
 
     [SerializeField]
@@ -25,12 +26,17 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField]
     private Sprite rightSprite;
 
+    [SerializeField]
+    private Sprite leftCrouchSprite;
+    [SerializeField]
+    private Sprite rightCrouchSprite;
 
     // Start is called before the first frame update
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
         sr = GetComponent<SpriteRenderer>();
+
     }
     
     // Update is called once per frame
@@ -52,11 +58,39 @@ public class PlayerMovement : MonoBehaviour
             jumpInputted = true;
         }
 
+        
+        if (Input.GetKey(KeyCode.S)) {
+            crouching = true;
+
+            if (sr.sprite == leftSprite) {
+                sr.sprite = leftCrouchSprite;
+            }
+            else if (sr.sprite == rightSprite) {
+                sr.sprite = rightCrouchSprite;
+            }
+
+            gameObject.GetComponent<BoxCollider2D>().size.Set(1.28f, 0.72f);
+        }
+        else {
+            crouching = false;
+
+            if (sr.sprite == leftCrouchSprite) {
+                sr.sprite = leftSprite;
+            }
+            else if (sr.sprite == rightCrouchSprite) {
+                sr.sprite = rightSprite;
+            }
+
+            gameObject.GetComponent<BoxCollider2D>().size.Set(1.28f, 1.28f);
+
+        }
+        
+
         if (direction.x < 0) {
-            sr.sprite = leftSprite;
+            sr.sprite = crouching ? leftCrouchSprite : leftSprite;
         }
         else if (direction.x > 0) {
-            sr.sprite = rightSprite;
+            sr.sprite = crouching ? rightCrouchSprite : rightSprite;
         }
     }
 
