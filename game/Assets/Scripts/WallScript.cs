6a78e83675a90e33ff48975105bb719e1a6c9e79
mikhailmelaku <mikhailmelaku11@gmail.com>
2020-02-14 13:52:54 -0800

    walls and spawns

---
+++ game/Assets/Scripts/WallScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallScript : MonoBehaviour
+{
+    [SerializeField]
+    private Sprite sp0;
+    [SerializeField]
+    private Sprite sp1;
+    [SerializeField]
+    private Sprite sp2;
+    [SerializeField]
+    private Sprite sp3;
+}
